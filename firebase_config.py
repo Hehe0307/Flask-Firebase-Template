c3d81f6ea2f1a2c51bfa72a62af3a17bcae49e77
Hehe0307 <127603791+Hehe0307@users.noreply.github.com>
2025-04-20 19:40:10 +0800

    Initialize Project
    Initialize the project following the youtube of https://youtu.be/HoRutj1z3fQ?si=Tx0BSDjjj1zdt6pd

--- firebase_config.py
+++ firebase_config.py
@@ -1,4 +1,10 @@
 # This file contains the firebase configuration details.
 firebaseConfig = {
-
+  "apiKey": "AIzaSyBD0-xlN8vBGgwtQP856q7shZk2geQtuYc",
+  "authDomain": "drone-management-platform.firebaseapp.com",
+  "projectId": "drone-management-platform",
+  "storageBucket": "drone-management-platform.firebasestorage.app",
+  "messagingSenderId": "278291832943",
+  "appId": "1:278291832943:web:cbf995a1443bbc8b8ded9a",
+  "measurementId": "G-CT6PT2X5D3"
 }
--- static/firebase-config.js
+++ static/firebase-config.js
@@ -5,13 +5,19 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBD0-xlN8vBGgwtQP856q7shZk2geQtuYc",
+  authDomain: "drone-management-platform.firebaseapp.com",
+  projectId: "drone-management-platform",
+  storageBucket: "drone-management-platform.firebasestorage.app",
+  messagingSenderId: "278291832943",
+  appId: "1:278291832943:web:cbf995a1443bbc8b8ded9a",
+  measurementId: "G-CT6PT2X5D3"
 };
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+// const provider = new GoogleAuthProvider();s
 
 const db = getFirestore(app);
 
